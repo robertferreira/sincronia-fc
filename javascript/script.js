6a437ab28e499b293c771fd52b8719740152e177
Robert Ferreira <robertferreira2001@hotmail.com>
2023-03-29 12:44:48 -0300

    update

--- javascript/script.js
+++ javascript/script.js
@@ -1,15 +1,19 @@
 const expandirCabecalho = function () {
     let cabecalho = document.getElementById('cabecalho');
     let logo_cabecalho = document.getElementById('logo-cabecalho');
+    let opcao_cabecalho = document.getElementsByClassName('menu-cabecalho-opcao');
 
     // EXPANDIR CABEÇALHO E LOGO
     cabecalho.addEventListener("mouseover", () => {
-      cabecalho.classList.add('cabecalho-expandido')
-      logo_cabecalho.setAttribute("id", "logo-cabecalho-expandido")
+      cabecalho.classList.add('cabecalho-expandido');
+      logo_cabecalho.setAttribute("id", "logo-cabecalho-expandido");
+      //opcao_cabecalho.item
     })
     cabecalho.addEventListener("mouseout", () => {
-      cabecalho.classList.remove('cabecalho-expandido')
-      logo_cabecalho.removeAttribute("id", "logo-cabecalho-expandido")
+      cabecalho.classList.remove('cabecalho-expandido');
+      logo_cabecalho.removeAttribute("id", "logo-cabecalho-expandido");
     })
+
+    //INSERIR NOMES
 }
 expandirCabecalho();
