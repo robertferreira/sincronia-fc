2efe042c70227fd3cc2e6a9155bd43fcce4349b8
Robert Ferreira <robertferreira2001@hotmail.com>
2023-04-16 17:23:06 -0300

    update

--- javascript/pages-js/home.js
+++ javascript/pages-js/home.js
@@ -1,9 +1,11 @@
 const dadosHome3Coluna = async function () {
 
     // ELEMENTOS UL / LI
-    var ul_membros = document.getElementById("home-3-membros");
-    var ul_gamertag = document.getElementById("home-3-gamertag");
-    var ul_games = document.getElementById("home-3-games");
+    let ul_membros = document.getElementById("home-3-membros");
+    let ul_gamertag = document.getElementById("home-3-gamertag");
+    let ul_games = document.getElementById("home-3-games");
+    let icon_membros ='<i class="fa-solid fa-circle-user"></i>';
+    let icon_gamertag ='<i class="fa-brands fa-xbox"></i>';
 
     // DADOS JSON
     const dados_jogadores = await fetch("./dados/jogadores.json");
@@ -13,8 +15,17 @@ const dadosHome3Coluna = async function () {
 
     // ADICIONAR INFORMAÇÕES NA HOME-3 COLUNA COM BASE NOS DADOS JSON
     jogadores.forEach(element => {
-        console.log(element.nome);
+        // INFORMAÇÕES DE MEMBROS
+        let li_membros = document.createElement('li');
+        li_membros.innerHTML = icon_membros + element.nome;
+        ul_membros.appendChild(li_membros);
+        // INFORMAÇÕES DE MEMBROS
+        let li_gamertag = document.createElement('li');
+        li_gamertag.innerHTML = icon_gamertag + element.gamertag;
+        ul_gamertag.appendChild(li_gamertag);
     });
+
+
 
 }
 dadosHome3Coluna();
