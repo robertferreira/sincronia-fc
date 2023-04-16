8c453174d2d48d9727c01b123067b8e4a40971b1
Robert Ferreira <robertferreira2001@hotmail.com>
2023-04-16 17:00:47 -0300

    update

--- javascript/pages-js/home.js
+++ javascript/pages-js/home.js
@@ -1,13 +1,20 @@
-const dadosHome3Coluna = async function(){
+const dadosHome3Coluna = async function () {
 
-const dados = await fetch("./dados/jogadores.json");
-const jogadores = await dados.json();
+    // ELEMENTOS UL / LI
+    var ul_membros = document.getElementById("home-3-membros");
+    var ul_gamertag = document.getElementById("home-3-gamertag");
+    var ul_games = document.getElementById("home-3-games");
 
-console.log(jogadores);
+    // DADOS JSON
+    const dados_jogadores = await fetch("./dados/jogadores.json");
+    const jogadores = await dados_jogadores.json();
+    const dados_games = await fetch("./dados/games.json");
+    const games = await dados_games.json();
 
-jogadores.forEach(element => {
-    console.log(element.nome);
-});
+    // ADICIONAR INFORMAÇÕES NA HOME-3 COLUNA COM BASE NOS DADOS JSON
+    jogadores.forEach(element => {
+        console.log(element.nome);
+    });
 
 }
 dadosHome3Coluna();
