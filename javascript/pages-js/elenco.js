9eca1c67c4b4653f44498d5fa221dbc1d41ef912
Robert Ferreira <robertferreira2001@hotmail.com>
2023-05-04 20:44:22 -0300

    update

--- javascript/pages-js/elenco.js
+++ javascript/pages-js/elenco.js
@@ -1,16 +1,9 @@
 const dadosTemporadas = async function () {
     const dados_temporadas = await fetch("../dados/temporadas.json");
     const temporadas = await dados_temporadas.json();
-    const icon_bola = '<i class="fa-regular fa-futbol"></i>';
-    const icon_taca = '<i class="fa-solid fa-trophy"></i>';
-    const icon_acessos = '<i class="fa-solid fa-circle-up"></i>';
-    const icon_rebaixamentos = '<i class="fa-solid fa-circle-down"></i>';
-    const icon_vitorias = '<i class="fa-solid fa-circle-check"></i>';
-    const icon_empates = '<i class="fa-solid fa-circle-minus"></i>';
-    const icon_derrotas = '<i class="fa-solid fa-circle-xmark"></i>';
 
     temporadas.forEach(element => {
-        var conteudo = document.getElementById('temporadas-2');
+        var conteudo = document.getElementById('elenco-2');
 
         //Create Card
         var card = document.createElement('div');
@@ -40,47 +33,23 @@ const dadosTemporadas = async function () {
         var card_statistics_table1_tr5 = document.createElement('tr');
         var card_statistics_table1_tr5_td1 = document.createElement('td');
         var card_statistics_table1_tr5_td2 = document.createElement('td');
-        var card_statistics_table2 = document.createElement('table');
-        var card_statistics_table2_tr1 = document.createElement('tr');
-        var card_statistics_table2_tr1_td1 = document.createElement('td');
-        var card_statistics_table2_tr1_td2 = document.createElement('td');
-        var card_statistics_table2_tr2 = document.createElement('tr');
-        var card_statistics_table2_tr2_td1 = document.createElement('td');
-        var card_statistics_table2_tr2_td2 = document.createElement('td');
-        var card_statistics_table2_tr3 = document.createElement('tr');
-        var card_statistics_table2_tr3_td1 = document.createElement('td');
-        var card_statistics_table2_tr3_td2 = document.createElement('td');
-        var card_statistics_table2_tr4 = document.createElement('tr');
-        var card_statistics_table2_tr4_td1 = document.createElement('td');
-        var card_statistics_table2_tr4_td2 = document.createElement('td');
         //Create H3
         var card_statistics_table1_tr1_td1_h3 = document.createElement('h3');
         var card_statistics_table1_tr1_td2_h3 = document.createElement('h3');
-        var card_statistics_table2_tr1_td1_h3 = document.createElement('h3');
-        var card_statistics_table2_tr1_td2_h3 = document.createElement('h3');
 
         //Conteúdo Card Title
-        card_title_h3.innerHTML = icon_bola + element.fifa;
+        card_title_h3.innerHTML = 'Nome do Jogador';
         //Conteúdo Card Statistics Table1
         card_statistics_table1_tr1_td1_h3.innerText = 'TEMPORADAS:';
         card_statistics_table1_tr1_td2_h3.innerText = element.temporadas;
-        card_statistics_table1_tr2_td1.innerHTML = icon_taca + 'Titulos de Liga:';
+        card_statistics_table1_tr2_td1.innerHTML = 'Titulos de Liga:';
         card_statistics_table1_tr2_td2.innerText = element.titulos_liga;
-        card_statistics_table1_tr3_td1.innerHTML = icon_taca + 'Titulos de Copa:';
+        card_statistics_table1_tr3_td1.innerHTML = 'Titulos de Copa:';
         card_statistics_table1_tr3_td2.innerText = element.titulos_copa;
-        card_statistics_table1_tr4_td1.innerHTML = icon_acessos + 'Acessos:';
+        card_statistics_table1_tr4_td1.innerHTML = 'Acessos:';
         card_statistics_table1_tr4_td2.innerText = element.acessos;
-        card_statistics_table1_tr5_td1.innerHTML = icon_rebaixamentos + 'Rebaixamentos:';
+        card_statistics_table1_tr5_td1.innerHTML = 'Rebaixamentos:';
         card_statistics_table1_tr5_td2.innerText = element.rebaixamentos;
-        //Conteúdo Card Statistics Table2
-        card_statistics_table2_tr1_td1_h3.innerText = 'JOGOS:';
-        card_statistics_table2_tr1_td2_h3.innerText = element.jogos;
-        card_statistics_table2_tr2_td1.innerHTML = icon_vitorias + 'Vitórias:';
-        card_statistics_table2_tr2_td2.innerText = element.vitorias;
-        card_statistics_table2_tr3_td1.innerHTML = icon_empates + 'Empates:';
-        card_statistics_table2_tr3_td2.innerText = element.empates;
-        card_statistics_table2_tr4_td1.innerHTML = icon_derrotas + 'Derrotas:';
-        card_statistics_table2_tr4_td2.innerText = element.derrotas;
 
         //Add Card
         conteudo.append(card);
@@ -89,7 +58,7 @@ const dadosTemporadas = async function () {
         card_title.append(card_title_h3);
         //Add Statistics
         //Table
-        card_statistics.append(card_statistics_table1,card_statistics_table2);
+        card_statistics.append(card_statistics_table1);
         //Table1 TRs
         card_statistics_table1.append(card_statistics_table1_tr1,card_statistics_table1_tr2,card_statistics_table1_tr3,card_statistics_table1_tr4,card_statistics_table1_tr5);
         //Table1 TDs
@@ -98,19 +67,6 @@ const dadosTemporadas = async function () {
         card_statistics_table1_tr3.append(card_statistics_table1_tr3_td1,card_statistics_table1_tr3_td2);
         card_statistics_table1_tr4.append(card_statistics_table1_tr4_td1,card_statistics_table1_tr4_td2);
         card_statistics_table1_tr5.append(card_statistics_table1_tr5_td1,card_statistics_table1_tr5_td2);
-        //Table2 TRs
-        card_statistics_table2.append(card_statistics_table2_tr1,card_statistics_table2_tr2,card_statistics_table2_tr3,card_statistics_table2_tr4);
-        //Table2 TDs
-        card_statistics_table2_tr1.append(card_statistics_table2_tr1_td1,card_statistics_table2_tr1_td2);
-        card_statistics_table2_tr2.append(card_statistics_table2_tr2_td1,card_statistics_table2_tr2_td2);
-        card_statistics_table2_tr3.append(card_statistics_table2_tr3_td1,card_statistics_table2_tr3_td2);
-        card_statistics_table2_tr4.append(card_statistics_table2_tr4_td1,card_statistics_table2_tr4_td2);
-        //Table1 TDs Dados
-        card_statistics_table1_tr1_td1.append(card_statistics_table1_tr1_td1_h3);
-        card_statistics_table1_tr1_td2.append(card_statistics_table1_tr1_td2_h3);
-        //Table2 TDs Dados
-        card_statistics_table2_tr1_td1.append(card_statistics_table2_tr1_td1_h3);
-        card_statistics_table2_tr1_td2.append(card_statistics_table2_tr1_td2_h3);
     });
 }
 dadosTemporadas();
