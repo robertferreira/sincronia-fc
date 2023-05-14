b37d9da26a86f1805fee73e39e4f44d0be2fdc6a
Robert Ferreira <robertferreira2001@hotmail.com>
2023-05-14 19:20:53 -0300

    update

--- javascript/pages-js/galeria.js
+++ javascript/pages-js/galeria.js
@@ -1,8 +1,33 @@
 const icon_fotos = '<i class="fa-solid fa-image"></i>';
 const icon_videos = '<i class="fa-brands fa-youtube"></i>';
 
-const carroselImagensGaleria = async function () {
-    const dados_imagens = await fetch("../dados/temporadas.json");
+const img = document.getElementById('img');
+const setaReturnImg = document.getElementById('seta-return-img');
+const setaNextImg = document.getElementById('seta-next-img');
+
+var cont_img = 0;
+
+/* Função Return Imagem */
+const returnImage = async function () {
+    const dados_imagens = await fetch("../dados/imagens.json");
     const imagens = await dados_imagens.json();
+
+    if (cont_img != 0){
+    cont_img--;
+    img.setAttribute('src', imagens[cont_img].imagem);
+    }
 }
-carroselImagensGaleria();
+
+/* Função Next Imagem */
+const nextImage = async function () {
+    const dados_imagens = await fetch("../dados/imagens.json");
+    const imagens = await dados_imagens.json();
+
+    if (cont_img != (imagens.length - 1)){
+        cont_img++;
+        img.setAttribute('src', imagens[cont_img].imagem);
+    }
+}
+
+setaReturnImg.addEventListener("click", () =>{returnImage()});
+setaNextImg.addEventListener("click", () =>{nextImage()});
